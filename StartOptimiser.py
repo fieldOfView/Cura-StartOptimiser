ce734fb0f639c0a72a408f2bbfb882b79b03621b
fieldOfView <aldo@fieldofview.com>
2020-04-16 21:50:29 +0200

    Add function to remove all but "generic" materials

--- StartOptimiser.py
+++ StartOptimiser.py
@@ -1,11 +1,8 @@
 # Copyright (c) 2020 fieldOfView
 # StartOptimiser is released under the terms of the AGPLv3 or higher.
 
-import os.path
-
 from UM.Extension import Extension
-from UM.Application import Application
-from UM.PluginRegistry import PluginRegistry
+from cura.CuraApplication import CuraApplication
 
 from PyQt5.QtCore import QObject
 
@@ -14,16 +11,16 @@ from . import LocalContainerProviderPatches
 from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
 
-
 class StartOptimiser(Extension, QObject,):
     def __init__(self, parent = None) -> None:
         QObject.__init__(self, parent)
         Extension.__init__(self)
 
-        self._application = Application.getInstance()
+        self._application = CuraApplication.getInstance()
 
         self.setMenuName(catalog.i18nc("@item:inmenu", "Startup Optimiser"))
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Disable loading unused configuration files"), self.optimiseStartup)
+        self.addMenuItem(catalog.i18nc("@item:inmenu", "Load only 'generic' materials"), self.removeBrandedMaterials)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Restore all configuration files"), self.resetOptimisations)
 
         self._local_container_provider_patches = None
@@ -33,7 +30,7 @@ class StartOptimiser(Extension, QObject,):
         self._application.getPreferences().addPreference("start_optimiser/id_blacklist", "")
 
     def _onPluginsLoaded(self) -> None:
-        local_container_provider = PluginRegistry.getInstance().getPluginObject("LocalContainerProvider")
+        local_container_provider = self._application.getPluginRegistry().getPluginObject("LocalContainerProvider")
         self._local_container_provider_patches = LocalContainerProviderPatches.LocalContainerProviderPatches(local_container_provider)
 
     def optimiseStartup(self) -> None:
@@ -72,6 +69,28 @@ class StartOptimiser(Extension, QObject,):
 
         unused_container_ids = local_container_ids - (active_stack_ids | active_definition_ids | active_container_ids)
         self._application.getPreferences().setValue("start_optimiser/id_blacklist", ";".join(list(unused_container_ids)))
+
+    def removeBrandedMaterials(self) -> None:
+        branded_materials = set()
+        keep_branded_materials = set()
+
+        container_registry = self._application.getContainerRegistry()
+        container_stacks = container_registry.findContainerStacks()
+
+        for stack in container_stacks:
+            if stack.getMetaDataEntry("type") not in ["machine", "extruder_train"]:
+                continue
+
+            if stack.material.getMetaDataEntry("brand", default="generic").lower() != "generic":
+                keep_branded_materials.add(stack.material.getMetaDataEntry("base_file"))
+
+        materials_metadata = container_registry.findInstanceContainersMetadata(type="material")
+        for metadata in materials_metadata:
+            if "brand" not in metadata or metadata["brand"].lower() != "generic":
+                branded_materials.add(metadata["base_file"])
+
+        unused_branded_materials = branded_materials - keep_branded_materials
+        self._application.getPreferences().setValue("start_optimiser/id_blacklist", ";".join(list(unused_branded_materials)))
 
     def resetOptimisations(self) -> None:
         self._application.getPreferences().setValue("start_optimiser/id_blacklist", "")
